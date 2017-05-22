c318e4ebe405fa3314229c3cf693357b57a66966
paweltar <paweltar@gmail.com>
2017-05-22 21:00:20 +0200

    Add AOS library for css animations

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,13 @@
 import $ from 'jquery';
+import 'aos/dist/aos.css';
+import AOS from 'aos'
 
 $(document).ready(function() {
+  AOS.init({
+      offset: 200,
+      duration: 600,
+      easing: 'ease-in-sine',
+      delay: 100,
+  });
   console.log('hello');
 });
