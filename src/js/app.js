ea60d67626939a5696ec3f68c6df4b26802b4250
paweltar <paweltar@gmail.com>
2017-05-23 11:35:36 +0200

    Add rellax library to project

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,17 @@
 import $ from 'jquery';
 import 'aos/dist/aos.css';
 import AOS from 'aos'
+import Rellax from 'rellax';
 
 $(document).ready(function() {
   AOS.init({
       offset: 200,
       duration: 600,
       easing: 'ease-in-sine',
-      delay: 100,
+  });
+
+  var rellax = new Rellax('.rellax', {
+    center: true
   });
   console.log('hello');
 });
