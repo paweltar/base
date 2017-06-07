3289a0a096ef31243aef4f2952a2d448d64f87a4
paweltar <paweltar@gmail.com>
2017-06-07 10:14:02 +0200

    Simplify starting layout

--- src/js/app.js
+++ src/js/app.js
@@ -4,15 +4,23 @@ import AOS from 'aos'
 import Rellax from 'rellax';
 
 $(window).on("load", function(){
-  AOS.init({
-      disable: window.innerWidth < 960,
-      offset: 200,
-      duration: 600,
-      easing: 'ease-in-sine',
-  });
 
-  var rellax = new Rellax('.rellax', {
-    center: true
-  });
+  // https://github.com/michalsnik/aos
+  // AOS example
+
+  // AOS.init({
+  //     disable: window.innerWidth < 960,
+  //     offset: 200,
+  //     duration: 600,
+  //     easing: 'ease-in-sine',
+  // });
+
+  // https://github.com/dixonandmoe/rellax
+  // Rellax example
+
+  // var rellax = new Rellax('.rellax', {
+  //   center: true
+  // });
+
   console.log('hello');
 });
